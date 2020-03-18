650e40b6b0c35d391bf0a7d8bfb925877633e896
GabrielGorog <gorog1338@yahoo.com>
2020-03-18 19:08:55 +0200

    added customer service and controller

--- src/main/java/org/fasttrackit/onlineshop/service/CustomerService.java
+++ src/main/java/org/fasttrackit/onlineshop/service/CustomerService.java
@@ -1,6 +1,49 @@
 package org.fasttrackit.onlineshop.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.fasttrackit.onlineshop.domain.Customer;
+import org.fasttrackit.onlineshop.exception.ResourceNotFoundException;
+import org.fasttrackit.onlineshop.persistance.CustomerRepository;
+import org.fasttrackit.onlineshop.transfer.customer.SaveCustomerRequest;
+import org.hibernate.validator.internal.util.logging.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.logging.Logger;
+
+@Service
 public class CustomerService {
 
-    private static final LOGGER
+    private static final Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
+
+    private final CustomerRepository customerRepository;
+    private final ObjectMapper objectMapper;
+
+    @Autowired
+    public CustomerService(CustomerRepository customerRepository, ObjectMapper objectMapper) {
+        this.customerRepository = customerRepository;
+
+        this.objectMapper = objectMapper;
+    }
+
+    public Customer createCustomer(SaveCustomerRequest request) {
+        LOGGER.info("Creating customer {}", request);
+
+        Customer customer = objectMapper.convertValue(request, Customer.class);
+
+        return customerRepository.save(customer);
+
+        public Customer getCustomer(long id) {
+            LOGGER.info("Retrieving customer {}", id);
+
+            return CustomerRepository.findById(id).orElseThrow(() - > new ResourceNotFoundException("" +
+                    "Customer" + id + " not found."));
+        }
+
+    }
+
+
+
+
+
 }
